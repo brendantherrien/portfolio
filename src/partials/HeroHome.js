234dfdd7deb5f337ee43ce78762def8c7c9aa5c8
Brendan Therrien <brendant98@icloud.com>
2021-04-19 23:45:04 -0400

    attempt resume link again

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -42,7 +42,7 @@ function HeroHome() {
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">View My Work</a>
                 </div>
                 <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href={resumePDF} target="_blank">See My Resume</a>
+                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href={process.env.PUBLIC_URL + "/misc/Resume.pdf"} target="_blank">See My Resume</a>
                 </div>
               </div>
             </div>
